0e5259483aaf64a84d948cee28c940e2ca4f9ad4
Yuliia Krasnozhon <ykrasnozhen@ukr.net>
2025-06-11 18:06:06 +0300

    fix css file

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -17,10 +17,10 @@ export function createGallery(images) {
           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
         <div class="info">
-          <p><b>Likes:</b> ${likes}</p>
-          <p><b>Views:</b> ${views}</p>
-          <p><b>Comments:</b> ${comments}</p>
-          <p><b>Downloads:</b> ${downloads}</p>
+          <p class="info-item"><b>Likes</b><span> ${likes}</span></p>
+          <p class="info-item"><b>Views</b><span> ${views}</span></p>
+          <p class="info-item"><b>Comments</b><span> ${comments}</span></p>
+          <p class="info-item"><b>Downloads</b><span> ${downloads}</span></p>
         </div>
       </li>
     `)
