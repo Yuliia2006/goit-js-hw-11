b30524b36362c38e825ae0418b0507d837200e4d
Yuliia Krasnozhon <ykrasnozhen@ukr.net>
2025-06-11 17:21:06 +0300

    add files

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_KEY = '50800171-f0005a0681d5ee847775b056c';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
